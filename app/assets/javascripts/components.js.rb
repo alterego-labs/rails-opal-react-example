63c329a9e6b05e32c31deb4351f62dfc50d49644
Sergey Gernyak <sergeg1990@gmail.com>
2015-06-08 13:01:54 +0300

    Use opal jquery syntax instead vanilla JS

--- app/assets/javascripts/components.js.rb
+++ app/assets/javascripts/components.js.rb
@@ -2,5 +2,5 @@ require 'opal-react'
 require 'components/hello_message'
 
 Document.ready? do
-  React.render React.create_element(HelloMessage), `document.getElementById('content')`
+  React.render React.create_element(HelloMessage), Element.find("#content")
 end
